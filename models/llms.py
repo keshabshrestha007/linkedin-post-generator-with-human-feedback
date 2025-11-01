167e760219be6db1efaf7b5ae9226e1f7c03bc9b
keshab shrestha <shresthakeshab083@gmail.com>
2025-11-01 11:05:44 +0545

    Validate GROQ_API_KEY in streamlit secrets
    Add check for GROQ_API_KEY in streamlit secrets

--- models/llms.py
+++ models/llms.py
@@ -1,13 +1,19 @@
 from langchain_groq import ChatGroq
 from dotenv import load_dotenv
 import os
-
+"""
 # Load environment variables
 load_dotenv()
 try:
     GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 except:
     raise ValueError("GROQ_API_KEY not found in environment variables.")
+"""
+
+if st.secrets["GROQ_API_KEY"] is None:
+    raise ValueError("GROQ_API_KEY variable is not streamlit.secrets.toml")
+
+api_key = st.secrets["GROQ_API_KEY"]
 
 def create_llm(temperature:float=0.3):
     """
